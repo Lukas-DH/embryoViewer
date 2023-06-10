9cae09d9a4719c469fabd5a817034cd6e765cc32
Lukas <lukas@open@ivf.com>
2023-06-10 10:15:07 +0200

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
