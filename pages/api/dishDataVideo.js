65220d086e5fd53b7289da6e5fd1988d0be425d6
Lukas <lukas@open@ivf.com>
2023-08-28 22:16:24 +0200

    testvideoid

--- pages/api/dishDataVideo.js
+++ pages/api/dishDataVideo.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
     // );
     // testxxx
     const videoID = data.content[0].dish_uuid;
-    console.log("videoID", videoID);
+    console.log("videoIDxxx", videoID);
     const response2 = await fetch(
       `https://10.13.45.201/geri_connect/public/api/v1/dishrecords?dish_uuid=${videoID}`,
       {
