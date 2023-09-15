f9db10d07f2bda717e3f20804ff3dc4298592e24
Lukas <lukas@open@ivf.com>
2023-09-15 10:24:52 +0200

    keyboard fix

--- components/QRCodeReader.js
+++ components/QRCodeReader.js
@@ -4,41 +4,22 @@ export default function ScannerInput({ onScanned }) {
   const [value, setValue] = useState("");
 
   useEffect(() => {
-    // function handleKeyDown(event) {
-    //   if (event.keyCode === 13) {
-    //     // Handle scanned value here
-    //     const scannedValue = value.trim();
-    //     console.log(`Scanned value: ${scannedValue}`);
-
-    //     // Call onScanned callback with scanned value
-    //     if (scannedValue) {
-    //       onScanned(scannedValue);
-    //     }
-
-    //     // Reset input value
-    //     setValue("");
-    //   } else {
-    //     // Update input value
-    //     setValue((value) => value + event.key);
-    //   }
-    // }
-
     function handleKeyDown(event) {
-      // Simulate the key press using the 'en-US' locale to get the character as it would appear on a US keyboard.
-      let char = new KeyboardEvent("keydown", {
-        key: event.key,
-        locale: "en-US",
-      }).key;
-
       if (event.keyCode === 13) {
+        // Handle scanned value here
         const scannedValue = value.trim();
         console.log(`Scanned value: ${scannedValue}`);
+
+        // Call onScanned callback with scanned value
         if (scannedValue) {
           onScanned(scannedValue);
         }
+
+        // Reset input value
         setValue("");
       } else {
-        setValue((value) => value + char);
+        // Update input value
+        setValue((value) => value + event.key);
       }
     }
 
